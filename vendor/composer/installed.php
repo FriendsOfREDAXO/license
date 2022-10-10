313f0af37fdabd7120e12b05eaca542376de73d1
Alexander Walther <aw@alexplus.de>
2022-10-10 08:55:40 +0200

    run composer, version bump, remove changelog.md

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,51 +1,50 @@
 <?php return array(
-  'root' =>
-  array(
-    'pretty_version' => '0.9.0',
-    'version' => '0.9.0.0',
-    'aliases' =>
-    array(
+    'root' => array(
+        'pretty_version' => '1.0',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'friendsofredaxo/license',
+        'dev' => true,
     ),
-    'reference' => null,
-    'name' => 'friendsofredaxo/license',
-  ),
-  'versions' =>
-  array(
-    'setasign/fpdf' =>
-    array(
-      'pretty_version' => '1.8.1',
-      'version' => '1.8.1.0',
-      'aliases' =>
-      array(
-      ),
-      'reference' => '2c68c9e6c034ac3187d25968790139a73184cdb1',
+    'versions' => array(
+        'friendsofredaxo/license' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'setasign/fpdf' => array(
+            'pretty_version' => '1.8.1',
+            'version' => '1.8.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../setasign/fpdf',
+            'aliases' => array(),
+            'reference' => '2c68c9e6c034ac3187d25968790139a73184cdb1',
+            'dev_requirement' => false,
+        ),
+        'symfony/finder' => array(
+            'pretty_version' => 'v4.4.44',
+            'version' => '4.4.44.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/finder',
+            'aliases' => array(),
+            'reference' => '66bd787edb5e42ff59d3523f623895af05043e4f',
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-php80' => array(
+            'pretty_version' => 'v1.26.0',
+            'version' => '1.26.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-php80',
+            'aliases' => array(),
+            'reference' => 'cfa0ae98841b9e461207c13ab093d76b0fa7bace',
+            'dev_requirement' => false,
+        ),
     ),
-    'symfony/finder' =>
-    array(
-      'pretty_version' => 'v4.4.44',
-      'version' => '4.4.44.0',
-      'aliases' =>
-      array(
-      ),
-      'reference' => '66bd787edb5e42ff59d3523f623895af05043e4f',
-    ),
-    'symfony/polyfill-php80' =>
-    array(
-      'pretty_version' => 'v1.26.0',
-      'version' => '1.26.0.0',
-      'aliases' =>
-      array(
-      ),
-      'reference' => 'cfa0ae98841b9e461207c13ab093d76b0fa7bace',
-    ),
-    'friendsofredaxo/license' =>
-    array(
-      'pretty_version' => '0.9.0',
-      'version' => '0.9.0.0',
-      'aliases' =>
-      array(
-      ),
-      'reference' => null,
-    ),
-  ),
 );
